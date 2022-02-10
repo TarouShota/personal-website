3ddd9dd0e5f4104849a312ad03ee19c01315177a
TarouShota <adilovbekzot@gmail.com>
2022-02-10 21:32:49 +0100

    bruh

--- script.js
+++ script.js
@@ -1,5 +1,56 @@
-function multiplication() {
-    console.log('Hello World');
-    
+
+let sayHello = (name) => {
+    let phrase = `Hello ${name}`;
+    say(phrase);
 }
-getElementbyId('div');
+
+let say = (phrase) => {
+    alert(`** ${phrase} **`);
+}
+
+
+//sayHello();
+
+let user = {
+    name: 'Shota',
+    age: 19
+};
+let clone = {};
+for (let key in user) {
+    clone[key] = user[key];
+}
+for (let key in clone) {
+    console.log(clone[key]);
+}
+//alert(clone.age);
+
+clone = Object.assign({}, user);
+
+let randomValue = { canWalk: true };
+
+console.log(`${clone.name}`);
+Object.assign(clone, randomValue);
+
+let deepEqual = (firstValue, secondValue) => {
+    if (firstValue === secondValue) {
+        return true
+    }
+    return false
+}
+//alert(deepEqual(user, clone));
+let userObj = {
+    name: "John",
+    sizes: {
+        height: 182,
+        width: 50
+    }
+};
+
+let cloneObj = Object.assign({}, userObj);
+
+console.log(userObj.sizes === cloneObj.sizes); // true, same object
+
+// user and clone share sizes
+userObj.sizes.width = userObj.sizes.width + 100;       // change a property from one place
+console.log(cloneObj.sizes.width); // 51, see the result from the other one
+console.log(cloneObj[0]);
